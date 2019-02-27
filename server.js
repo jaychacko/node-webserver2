44ddf0d1e0537c314152a5e7f06f644593762ad4
jay chacko <chackojayui@gmail.com>
2019-02-26 21:40:15 -0600

    updated server

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const hbs = require('hbs')
 const fs = require('fs')
 
+const port = process.env.PORT || 5000;
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials')
@@ -57,6 +58,6 @@ app.get('/bad', (req, res) => {
         error: 'unable to respond to server'
     })
 })
-app.listen(5000, () => {
-    console.log('server is up on 5000')
+app.listen(port, () => {
+    console.log(`server is up on ${port}`)
 });
